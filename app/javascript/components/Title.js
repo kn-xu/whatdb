dd9568495e2360e5c51a21fa8e671508fb4f45be
Kevin Xu <xuboxes@gmail.com>
2018-06-19 01:50:58 -0700

    Initial commit with home page with rails + react

---
+++ app/javascript/components/Title.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Title extends React.Component {
+    render() {
+        return (
+            <div className="flex-center position-ref full-height">
+                <div className="content">
+                    <div className="title m-b-md">
+                        Need helping selecting a database to use?
+                    </div>
+
+                    <div className="links">
+                        <a href="">Begin</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Title
